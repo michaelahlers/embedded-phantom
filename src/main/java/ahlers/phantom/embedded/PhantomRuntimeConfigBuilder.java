bc7f84c8c649240cc804ed18a0179607a2bd1033
Michael Ahlers <michael@ahlers.co>
2016-08-13 19:17:25 -0400

    Add runtime config. class

---
+++ src/main/java/ahlers/phantom/embedded/PhantomRuntimeConfigBuilder.java
@@ -0,0 +1,22 @@
+package ahlers.phantom.embedded;
+
+import de.flapdoodle.embed.process.config.RuntimeConfigBuilder;
+import de.flapdoodle.embed.process.runtime.ICommandLinePostProcessor;
+
+/**
+ * @author [[mailto:michael@ahlers.consulting Michael Ahlers]]
+ */
+public class PhantomRuntimeConfigBuilder
+        extends RuntimeConfigBuilder {
+
+    public RuntimeConfigBuilder defaults() {
+        processOutput().setDefault(PhantomProcessOutputConfig.getDefaultInstance());
+
+        commandLinePostProcessor().setDefault(new ICommandLinePostProcessor.Noop());
+
+        artifactStore().setDefault(new PhantomExtractedArtifactStoreBuilder().defaults().build());
+
+        return this;
+    }
+
+}
