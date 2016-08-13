3e6bc335095af0aa4df15597885c55c9c5b459ee
Michael Ahlers <michael@ahlers.co>
2016-08-13 19:13:16 -0400

    Refine temp. naming

--- src/main/java/ahlers/phantom/embedded/PhantomDownloadConfigBuilder.java
+++ src/main/java/ahlers/phantom/embedded/PhantomDownloadConfigBuilder.java
@@ -6,26 +6,33 @@ import de.flapdoodle.embed.process.extract.UUIDTempNaming;
 import de.flapdoodle.embed.process.io.directories.UserHome;
 import de.flapdoodle.embed.process.io.progress.StandardConsoleProgressListener;
 
+import java.nio.file.Paths;
+
 /**
  * @author [[mailto:michael@ahlers.consulting Michael Ahlers]]
  */
 public class PhantomDownloadConfigBuilder
         extends DownloadConfigBuilder {
 
-    public PhantomDownloadConfigBuilder() {
+    public PhantomDownloadConfigBuilder defaults() {
+        /* Names temporary, in-progress downloads. */
         fileNaming().setDefault(new UUIDTempNaming());
+        downloadPrefix().setDefault(new DownloadPrefix("downloads"));
 
         /* Official download site appears problematic for programmatic user agents. See michaelahlers/specter#1 (and ariya/phantomjs#13951). */
 
         // downloadPath().setDefault(new DownloadPath("https://bitbucket.org/ariya/phantomjs/downloads/"));
         // downloadPath().setDefault(new DownloadPath("https://cnpmjs.org/downloads/"));
         downloadPath().setDefault(new DownloadPath("https://github.com/paladox/phantomjs/releases/download/2.1.7/"));
+        packageResolver().setDefault(PhantomPackageResolver.getInstance());
+
+        /* Defines where completed download files will land. */
+        artifactStorePath().setDefault(new UserHome(Paths.get(".embedded-phantom", "downloads").toString()));
 
         progressListener().setDefault(new StandardConsoleProgressListener());
-        packageResolver().setDefault(PhantomPackageResolver.getInstance());
-        artifactStorePath().setDefault(new UserHome(".embedded-phantom"));
-        downloadPrefix().setDefault(new DownloadPrefix("phantom-download"));
         userAgent().setDefault(new UserAgent("Mozilla/5.0 (compatible Embedded Phantom +https://github.com/michaelahlers/embedded-phantom)"));
+
+        return this;
     }
 
 }
