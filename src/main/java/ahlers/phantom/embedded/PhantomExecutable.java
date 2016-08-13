6824bbb9e822920f6818bf29fa5661ca5438edaa
Michael Ahlers <michael@ahlers.co>
2016-08-13 12:42:04 -0400

    Initial bootstrap work

---
+++ src/main/java/ahlers/phantom/embedded/PhantomExecutable.java
@@ -0,0 +1,42 @@
+package ahlers.phantom.embedded;
+
+import de.flapdoodle.embed.process.config.IRuntimeConfig;
+import de.flapdoodle.embed.process.distribution.Distribution;
+import de.flapdoodle.embed.process.extract.IExtractedFileSet;
+import de.flapdoodle.embed.process.runtime.Executable;
+import org.slf4j.Logger;
+
+import java.io.IOException;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
+/**
+ * @author [[mailto:michael@ahlers.consulting Michael Ahlers]]
+ */
+public class PhantomExecutable
+        extends Executable<PhantomConfig, PhantomProcess> {
+
+    private final static Logger logger = getLogger(PhantomExecutable.class);
+
+    private final IExtractedFileSet files;
+
+    PhantomExecutable(
+            final Distribution distribution,
+            final PhantomConfig config,
+            final IRuntimeConfig runtimeConfig,
+            final IExtractedFileSet files
+    ) {
+        super(distribution, config, runtimeConfig, files);
+        this.files = files;
+    }
+
+    @Override
+    protected PhantomProcess start(
+            final Distribution distribution,
+            final PhantomConfig config,
+            final IRuntimeConfig runtimeConfig
+    ) throws IOException {
+        return new PhantomProcess(distribution, config, runtimeConfig, this);
+    }
+
+}
---
+++ src/main/java/ahlers/phantom/embedded/PhantomProcess.java
@@ -0,0 +1,52 @@
+package ahlers.phantom.embedded;
+
+import de.flapdoodle.embed.process.config.IRuntimeConfig;
+import de.flapdoodle.embed.process.distribution.Distribution;
+import de.flapdoodle.embed.process.extract.IExtractedFileSet;
+import de.flapdoodle.embed.process.runtime.AbstractProcess;
+import org.slf4j.Logger;
+
+import java.io.IOException;
+import java.util.List;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
+/**
+ * @author [[mailto:michael@ahlers.consulting Michael Ahlers]]
+ */
+public class PhantomProcess
+        extends AbstractProcess<PhantomConfig, PhantomExecutable, PhantomProcess> {
+
+    private final static Logger logger = getLogger(PhantomProcess.class);
+
+    public PhantomProcess(
+            final Distribution distribution,
+            final PhantomConfig config,
+            final IRuntimeConfig runtimeConfig,
+            final PhantomExecutable executable)
+            throws IOException {
+        super(distribution, config, runtimeConfig, executable);
+    }
+
+    @Override
+    protected List<String> getCommandLine(
+            final Distribution distribution,
+            final PhantomConfig config,
+            final IExtractedFileSet executable
+    ) throws IOException {
+        return PhantomCommand
+                .valueFor(distribution)
+                .emit(config, executable);
+    }
+
+    @Override
+    protected void stopInternal() {
+
+    }
+
+    @Override
+    protected void cleanupInternal() {
+
+    }
+
+}
