fe0344309db5b51b11f6281a0f1b997c6396004b
Michael Ahlers <michael@ahlers.co>
2016-08-17 11:10:34 -0400

    Verify standard input correctly reaches the hidden Process instance (for #4)

--- src/test/scala/ahlers/phantom/embedded/PhantomProcessSpec.scala
+++ src/test/scala/ahlers/phantom/embedded/PhantomProcessSpec.scala
@@ -1,7 +1,22 @@
 package ahlers.phantom.embedded
 
+import java.io.File
+
+import com.google.common.collect.ImmutableList
+import de.flapdoodle.embed.process.config.io.ProcessOutput
+import de.flapdoodle.embed.process.config.{IRuntimeConfig, ISupportConfig}
+import de.flapdoodle.embed.process.distribution.Platform._
+import de.flapdoodle.embed.process.distribution.{Distribution, IVersion}
+import de.flapdoodle.embed.process.extract.IExtractedFileSet
+import de.flapdoodle.embed.process.io.{IStreamProcessor, Processors}
+import de.flapdoodle.embed.process.runtime.ICommandLinePostProcessor
 import org.scalamock.scalatest.MockFactory
+import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.time.{Millis, Seconds, Span}
 import org.scalatest.{FlatSpec, Matchers, PrivateMethodTester}
+
+import scala.collection.mutable
+import scala.concurrent.Promise
 
 /**
  * @author [[mailto:michael@ahlers.consulting Michael Ahlers]]
@@ -10,6 +25,78 @@ class PhantomProcessSpec
   extends FlatSpec
           with PrivateMethodTester
           with Matchers
+          with ScalaFutures
           with MockFactory {
+
+  override implicit def patienceConfig: PatienceConfig = PatienceConfig(timeout = Span(5, Seconds), interval = Span(500, Millis))
+
+  it must "provide standard input" in {
+
+    val distribution = Distribution.detectFor(mock[IVersion])
+
+    val executable =
+      distribution.getPlatform match {
+        case Windows => new File("more")
+        case _ => new File("cat")
+      }
+
+    val commandFormatter = mock[IPhantomCommandFormatter]
+    (commandFormatter.format _).expects(*, *).returns(ImmutableList.of(executable.getPath)).atLeastOnce()
+
+    val config = mock[IPhantomConfig]
+    (config.formatter _).expects().returns(commandFormatter).atLeastOnce()
+    (config.debug _).expects().never()
+    (config.script _).expects().never()
+    (config.supportConfig _).expects().returns(mock[ISupportConfig]).atLeastOnce()
+
+    val expected = (0 until 10) map { i => s"message $i" }
+
+    val actual: Promise[List[String]] = Promise()
+
+    val processOutput = new ProcessOutput(
+      new IStreamProcessor {
+
+        val blocks: mutable.Buffer[String] = mutable.Buffer.empty
+
+        override def process(block: String): Unit = {
+          blocks ++= block.split('\n')
+          if (expected.size == blocks.size) actual.success(blocks.toList)
+        }
+
+        override def onProcessed(): Unit = ()
+
+      },
+      Processors.silent,
+      Processors.silent
+    )
+
+    val runtimeConfig = mock[IRuntimeConfig]
+    (runtimeConfig.isDaemonProcess _).expects().returns(false).atLeastOnce()
+    (runtimeConfig.getProcessOutput _).expects().returns(processOutput).atLeastOnce()
+    (runtimeConfig.getCommandLinePostProcessor _).expects().returns(new ICommandLinePostProcessor.Noop).atLeastOnce()
+
+    val files = mock[IExtractedFileSet]
+    (files.executable _).expects().returns(executable).atLeastOnce()
+
+    val process =
+      new PhantomProcess(
+        distribution,
+        config,
+        runtimeConfig,
+        new PhantomExecutable(
+          distribution,
+          config,
+          runtimeConfig,
+          files
+        )
+      )
+
+    val input = process.getStandardInput
+
+    expected.map(_ + '\n').foreach(input.write)
+    input.flush()
+
+    actual.future.futureValue should contain theSameElementsInOrderAs expected
+  }
 
 }
