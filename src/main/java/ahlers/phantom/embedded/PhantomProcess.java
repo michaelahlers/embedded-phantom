c6f6fe46b020fde427300c3d472c8fdcdc4af186
Michael Ahlers <michael@ahlers.co>
2016-08-24 12:14:54 -0400

    Perform initialization prior to instantiating the process (for #31)

--- src/main/java/ahlers/phantom/embedded/PhantomProcess.java
+++ src/main/java/ahlers/phantom/embedded/PhantomProcess.java
@@ -3,6 +3,7 @@ package ahlers.phantom.embedded;
 import ahlers.phantom.embedded.processes.IPhantomConsole;
 import ahlers.phantom.embedded.processes.WriterPhantomConsole;
 import com.google.common.collect.ImmutableList;
+import de.flapdoodle.embed.process.config.IExecutableProcessConfig;
 import de.flapdoodle.embed.process.config.IRuntimeConfig;
 import de.flapdoodle.embed.process.distribution.Distribution;
 import de.flapdoodle.embed.process.extract.IExtractedFileSet;
@@ -30,9 +31,109 @@ import static org.slf4j.LoggerFactory.getLogger;
  * @author [[mailto:michael@ahlers.consulting Michael Ahlers]]
  */
 public class PhantomProcess
-        extends AbstractProcess<IPhantomProcessConfig, PhantomExecutable, PhantomProcess> {
+        implements IPhantomProcess {
 
     private final static Logger logger = getLogger(PhantomProcess.class);
+
+    /**
+     * Provided for debugging.
+     */
+    private final ImmutableList<String> command;
+
+    /**
+     * Because {@link AbstractProcess}'s constructor starts the process immediately&mdash;invoking {@link AbstractProcess#getCommandLine(Distribution, IExecutableProcessConfig, IExtractedFileSet)}&mdash;there's no way to provide additional details needed. A prior implementation of this class used reflection to get extra values from the parent object's {@link PhantomExecutable} instance when generating the command line. Now using delegation the constructor is able to initialize class members that can be read when the delegateProcess starts, removing the reflective calls and setting up the instance entirely before starting the Phantom process.
+     */
+    private class DelegateProcess
+            extends AbstractProcess<IPhantomProcessConfig, PhantomExecutable, IPhantomProcess> {
+
+        public DelegateProcess(Distribution distribution, IPhantomProcessConfig config, IRuntimeConfig runtimeConfig, PhantomExecutable executable) throws IOException {
+            super(distribution, config, runtimeConfig, executable);
+        }
+
+        /**
+         * A trivial reader method for {@link PhantomProcess#command}.
+         */
+        @Override
+        protected List<String> getCommandLine(
+                final Distribution distribution,
+                final IPhantomProcessConfig processConfig,
+                final IExtractedFileSet files
+        ) throws IOException {
+            return command;
+        }
+
+
+        /*
+         * About reflective access to private members:
+         *
+         * AbstractProcess makes a mistake in calling overridden (virtual) methods from its constructor. This defeats the ability of subclasses to provide additional details to those methods, which this implementation needs to do (<em>e.g.</em>, IPhantomCommandFormatter).
+         *
+         * For more information see these answers on Stack Overflow:
+         *
+         * - http://stackoverflow.com/a/2529618/700420
+         * - http://stackoverflow.com/a/2529661/700420
+         *
+         * These explain the issue, and refer to work by Joshua Bloch and Neal Grafter.
+         */
+
+        private ProcessControl processControl() throws Exception {
+            return (ProcessControl) FieldUtils
+                    .getDeclaredField(AbstractProcess.class, "process", true)
+                    .get(this);
+        }
+
+        private Process nativeProcess() throws Exception {
+            return (Process) FieldUtils
+                    .getDeclaredField(ProcessControl.class, "process", true)
+                    .get(processControl());
+        }
+
+        @Override
+        protected void stopInternal() {
+            try {
+                logger.info("Sending exit command.");
+
+                final ILocalPhantomConsole console = consoleHolder.get();
+
+                console.flush();
+                console.write("//\n;phantom.exit();\n");
+                console.flush();
+
+                logger.info("Closing console input (future expressions will fail.");
+
+                console.destroy();
+
+            } catch (final Throwable t) {
+                logger.warn("Error issuing exit command (will attempt to kill the process).", t);
+            } finally {
+                sendKillToProcess();
+                tryKillToProcess();
+
+                logger.info("Waiting for process to exit.");
+
+                try {
+                    final Process process = nativeProcess();
+                    process.waitFor();
+                    logger.info(String.format("Received exit code %s (command was: %s).",
+                            process.exitValue(),
+                            StringUtils.join(command)
+                    ));
+                } catch (final Throwable t) {
+                    logger.warn("Error waiting for process to exit.", t);
+                }
+
+            /* Almost always throws an exception, even if the process successfully terminated. */
+                // stopProcess();
+            }
+        }
+
+        @Override
+        protected void cleanupInternal() {
+        }
+
+    }
+
+    private final DelegateProcess delegateProcess;
 
     public PhantomProcess(
             final Distribution distribution,
@@ -40,69 +141,12 @@ public class PhantomProcess
             final IRuntimeConfig runtimeConfig,
             final PhantomExecutable executable
     ) throws IOException {
-        super(distribution, processConfig, runtimeConfig, executable);
+        final IPhantomCommandFormatter commandFormatter = executable.commandFormatter();
+
+        this.command = commandFormatter.format(distribution, executable.getFile(), processConfig);
+        this.delegateProcess = new DelegateProcess(distribution, processConfig, runtimeConfig, executable);
+
         /* Throw no exceptions here unless finally terminating the process created by the super class. */
-    }
-
-    /*
-     * About reflective access to private members:
-     *
-     * AbstractProcess makes a mistake in calling overridden (virtual) methods from its constructor. This defeats the ability of subclasses to provide additional details to those methods, which this implementation needs to do (<em>e.g.</em>, IPhantomCommandFormatter).
-     *
-     * For more information see these answers on Stack Overflow:
-     *
-     * - http://stackoverflow.com/a/2529618/700420
-     * - http://stackoverflow.com/a/2529661/700420
-     *
-     * These explain the issue, and refer to work by Joshua Bloch and Neal Grafter.
-     */
-
-    private ProcessControl processControl() throws Exception {
-        return (ProcessControl) FieldUtils
-                .getDeclaredField(AbstractProcess.class, "process", true)
-                .get(this);
-    }
-
-    private Process nativeProcess() throws Exception {
-        return (Process) FieldUtils
-                .getDeclaredField(ProcessControl.class, "process", true)
-                .get(processControl());
-    }
-
-    /**
-     * Even though the <em>constructor</em> has access to the provided {@link PhantomExecutable} instance, any field initializer would not be called until <em>after</em> {@link #getCommandLine(Distribution, IPhantomProcessConfig, IExtractedFileSet)}. Therefore, it's necessary to reflect into the superclass for a reference that has been initialized so its properties can be provided to the command line generator.
-     */
-    private PhantomExecutable executable() throws Exception {
-        return (PhantomExecutable) FieldUtils
-                .getDeclaredField(AbstractProcess.class, "executable", true)
-                .get(this);
-    }
-
-    private IPhantomCommandFormatter commandFormatter() throws Exception {
-        return executable().commandFormatter();
-    }
-
-    /**
-     * Provided for debugging. <del>Guaranteed to be set only once.</del> Will be {@code null} until {@link #getCommandLine(Distribution, IPhantomProcessConfig, IExtractedFileSet)} is called by the super constructor.
-     */
-    private ImmutableList<String> command;
-
-    /**
-     * Internal API.
-     */
-    @Override
-    protected List<String> getCommandLine(
-            final Distribution distribution,
-            final IPhantomProcessConfig processConfig,
-            final IExtractedFileSet files
-    ) throws IOException {
-        try {
-            final ImmutableList<String> command = commandFormatter().format(distribution, files, processConfig);
-            this.command = command;
-            return command;
-        } catch (final Throwable t) {
-            throw new IOException("Error retrieving command formatter from executable.", t);
-        }
     }
 
     /**
@@ -114,7 +158,7 @@ public class PhantomProcess
 
     PrintWriter standardInputWriter() throws Exception {
         try {
-            return new PrintWriter(new OutputStreamWriter(nativeProcess().getOutputStream()));
+            return new PrintWriter(new OutputStreamWriter(delegateProcess.nativeProcess().getOutputStream()));
         } catch (final Throwable t) {
             throw new Exception("Error creating standard input processor.", t);
         }
@@ -171,6 +215,11 @@ public class PhantomProcess
                 }
             };
 
+    @Override
+    public boolean isProcessRunning() {
+        return delegateProcess.isProcessRunning();
+    }
+
     /**
      * Provides console access to {@code this} process instance. While {@link IPhantomConsole} describes intent, it's important to understand behaviors given this implementation. There are no guarantees regarding evaluation order when multiple threads use this interface. Ordering (by means of a {@linkplain BlockingQueue queue}, for example) are a higher-level concern. It's possible for blocks sent to {@link IPhantomConsole#write(String)} to interleave, creating invalid expressions or unintended results. There are likewise no guarantees (and cannot be) regarding output order simply by virtue of concurrent features of JavaScript.
      */
@@ -179,46 +228,8 @@ public class PhantomProcess
     }
 
     @Override
-    protected void stopInternal() {
-        try {
-            logger.info("Sending exit command.");
-
-            final ILocalPhantomConsole console = consoleHolder.get();
-
-            console.flush();
-            console.write("//\n;phantom.exit();\n");
-            console.flush();
-
-            logger.info("Closing console input (future expressions will fail.");
-
-            console.destroy();
-
-        } catch (final Throwable t) {
-            logger.warn("Error issuing exit command (will attempt to kill the process).", t);
-        } finally {
-            sendKillToProcess();
-            tryKillToProcess();
-
-            logger.info("Waiting for process to exit.");
-
-            try {
-                final Process process = nativeProcess();
-                process.waitFor();
-                logger.info(String.format("Received exit code %s (command was: %s).",
-                        process.exitValue(),
-                        StringUtils.join(command)
-                ));
-            } catch (final Throwable t) {
-                logger.warn("Error waiting for process to exit.", t);
-            }
-
-            /* Almost always throws an exception, even if the process successfully terminated. */
-            // stopProcess();
-        }
-    }
-
-    @Override
-    protected void cleanupInternal() {
+    public void stop() {
+        delegateProcess.stop();
     }
 
 }
