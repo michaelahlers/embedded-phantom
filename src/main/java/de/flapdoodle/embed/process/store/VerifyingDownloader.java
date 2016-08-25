a42d904cb713370af879d3a2c00175562de5bc94
Michael Ahlers <michael@ahlers.co>
2016-08-25 19:08:26 -0400

    Delegating downloader performing verification on completion (for #33)

---
+++ src/main/java/de/flapdoodle/embed/process/store/VerifyingDownloader.java
@@ -0,0 +1,46 @@
+package de.flapdoodle.embed.process.store;
+
+import de.flapdoodle.embed.process.config.store.IDownloadConfig;
+import de.flapdoodle.embed.process.distribution.Distribution;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Verifies downloaded files (typically for integrity) before returning their references.
+ *
+ * @author [[mailto:michael@ahlers.consulting Michael Ahlers]]
+ */
+public class VerifyingDownloader
+        implements IDownloader {
+
+    private final IDownloadVerifier verifier;
+
+    private final IDownloader delegate;
+
+    /**
+     * @param verifier Verification strategy.
+     * @param delegate Specific download strategy.
+     */
+    public VerifyingDownloader(final IDownloadVerifier verifier, final IDownloader delegate) {
+        this.verifier = verifier;
+        this.delegate = delegate;
+    }
+
+    @Override
+    public String getDownloadUrl(final IDownloadConfig runtime, final Distribution distribution) {
+        return delegate.getDownloadUrl(runtime, distribution);
+    }
+
+    @Override
+    public File download(final IDownloadConfig runtime, final Distribution distribution) throws IOException {
+        final File file = delegate.download(runtime, distribution);
+
+        if (!verifier.isValid(file)) {
+            throw new InvalidDownloadException(file);
+        }
+
+        return file;
+    }
+
+}
